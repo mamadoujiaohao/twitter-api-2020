f395baab5a1f13cbe905eb36f8b7a517ef0ff818
aking4511 <aking4511@gmail.com>
2023-06-08 15:41:19 +0800

    feat: git pull feature

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,13 @@ const { apiErrorHandler } = require('../middleware/error-handler')
 // require routes modules
 const admin = require('./modules/admin')
 const users = require('./modules/users')
+const tweets = require('./modules/tweets')
 
 // use router
 router.use('/api/admin', admin)
 router.use('/api/users', users)
+router.use('/api/tweets', tweets)
+router.get('/', (req, res) => res.redirect('/tweets'))
 router.use('/', apiErrorHandler)
 
 module.exports = router
